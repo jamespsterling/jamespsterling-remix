4a043120e9f25ebfb2aa39b9e22a4eec7edd3a94
James Sterling <jamespsterling@gmail.com>
2023-08-13 11:31:23 -0400

    Add skills links and styling

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -1,13 +1,17 @@
+import { Link } from '@remix-run/react';
 import { data } from '~/data/portfolio';
 
 export default function Skills() {
-
   return (
     <div id="skills">
       <h2 className="heading">Skills</h2>
       <ul>
         {data.skills.map((skill, i) => (
-          <li key={i}>{skill}</li>
+          <li>
+            <Link to={skill.link} target="_blank" key={i}>
+              {skill.label}
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
