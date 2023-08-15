f7ee8d3f4177d79775b5ceba0e51ee0e2156fcb2
James Sterling <jamespsterling@gmail.com>
2023-08-15 11:51:41 -0400

    Image lazy loading with spinner

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,9 +1,43 @@
 import { Link } from '@remix-run/react';
+import { useState } from 'react';
 import { ReactSVG } from 'react-svg';
 import { data } from '~/data/portfolio';
 
-export default function Projects() {
+function ProjectImage({ image }: { image: (typeof data.projects)[0]['image'] }) {
+  const [loading, setLoading] = useState(true);
   const imageExtension = (src: string) => src.split('.').pop();
+
+  return (
+    <>
+      {imageExtension(image.src) === 'svg' ? (
+        <div className="svg-wrapper">
+          <ReactSVG src={image.src} />
+        </div>
+      ) : (
+        <>
+          <img
+            src={image.src}
+            style={{
+              height: loading ? 0 : 'auto',
+              opacity: loading ? 0 : 1,
+              transition: '0.5s all',
+            }}
+            alt={`${image.src} image`}
+            onLoad={() => setLoading(false)}
+            loading="lazy"
+          />
+          {loading && (
+            <div className="loading">
+              <img src="/img/loading.svg" alt="loading..." />
+            </div>
+          )}
+        </>
+      )}
+    </>
+  );
+}
+
+export default function Projects() {
   const kebabCase = (name: string) => name.toLocaleLowerCase().replaceAll(' ', '-');
 
   return (
@@ -21,13 +55,7 @@ export default function Projects() {
               <div className="project shadow-large">
                 <div className="project-image">
                   <div className={project.image.class}>
-                    {imageExtension(project.image.src) === 'svg' ? (
-                      <div className="svg-wrapper">
-                        <ReactSVG src={project.image.src} />
-                      </div>
-                    ) : (
-                      <img src={project.image.src} alt={`${project.name} primary image`} />
-                    )}
+                    <ProjectImage image={project.image} />
                   </div>
                 </div>
                 <div className="project-info">
