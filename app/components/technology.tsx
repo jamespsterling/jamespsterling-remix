85680d13ad29d16329bb6fec271252a246dff429
James Sterling <jamespsterling@gmail.com>
2023-08-31 09:47:17 -0400

    Add technology icons

--- app/components/technology.tsx
+++ app/components/technology.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from '@remix-run/react';
 import type { Technology } from '~/data/types';
 
@@ -5,7 +6,8 @@ export default function Technology({ technology }: { technology: Technology }) {
   return (
     <div id="technology">
       <Link to={technology.link} target="_blank" rel="noopener">
-        {technology.label}
+        <span className="mr-2">{technology.label}</span>
+        <FontAwesomeIcon icon={technology.icon} />
       </Link>
     </div>
   );
--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
-import { logDevReady } from "@remix-run/cloudflare";
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-import * as build from "@remix-run/dev/server-build";
+import { logDevReady } from '@remix-run/cloudflare';
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
+import * as build from '@remix-run/dev/server-build';
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   logDevReady(build);
 }
 
+// Context: https://github.com/remix-run/remix/issues/6868
 export const onRequest = createPagesFunctionHandler({
   build,
   getLoadContext: (context) => ({ env: context.env }),
