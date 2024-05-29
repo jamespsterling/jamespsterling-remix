1640cd5e7f3438ddb0a22800523975c5ac6b9cad
James Sterling <jamespsterling@gmail.com>
2024-05-29 09:40:46 -0400

    Update nodejs to v22, cleanup

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -6,7 +6,7 @@ export default function About({ about }: { about: About }) {
       <div className="container mx-auto">
         <div className="grid xs:grid-cols-1 md:grid-cols-[30vw_1fr] gap-1">
           <div className="text-center">
-            <h2 className="heading">About Me</h2>
+            <h2 className="heading">About</h2>
           </div>
           <div className="">
             <p>{about.body}</p>
--- app/data/portfolio.ts
+++ app/data/portfolio.ts
@@ -60,7 +60,7 @@ import type { Project } from './types';
 const about = {
   name: 'James Sterling',
   title: 'Fullstack Software Engineer',
-  body: 'Full-stack software engineer with 14+ years of experience crafting software for SaaS platforms, enterprise deployments, and everything in between. My focus is to leverage the entire stack, back-end, middleware, and front-end to deliver thoughtful UI/UX experiences. My passion for self-documenting, scalable, and robust software orchestrations drives me to learn from and coach other engineers in best practices, refine developer experience (DX) tooling, and continuously hone my craft as a curious engineer.',
+  body: 'Full-stack software engineer with 15 years of experience crafting software for SaaS platforms, enterprise deployments, and everything in between. My focus is to leverage the entire stack, back-end, middleware, and front-end to deliver thoughtful UI/UX experiences. My passion for self-documenting, scalable, and robust software orchestrations drives me to learn from and coach other engineers in best practices, refine developer experience (DX) tooling, and continuously hone my craft as a curious engineer.',
 };
 const education = {
   university: {
