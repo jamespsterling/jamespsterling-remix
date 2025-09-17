40680f423afa01bb45d420ebe5809f1ebcbc9099
James Sterling <jamespsterling@gmail.com>
2025-09-17 10:19:28 -0400

    chore: remove Prettier dependencies and config files
    - Remove prettier and prettier-plugin-tailwindcss packages
    - Remove .prettierrc and prettier.config.js configuration files
    - Keep @svgr/plugin-prettier for SVG component formatting
    - Use Biome exclusively for code formatting and linting
    - Reduces bundle size and eliminates tool redundancy

--- prettier.config.js
+++
@@ -1,6 +0,0 @@
-// TODO fix ESM errors, https://remix.run/docs/en/1.19.3/pages/gotchas#importing-esm-packages
-// https://github.com/tailwindlabs/prettier-plugin-tailwindcss
-// module.exports = {
-//   plugins: ['prettier-plugin-tailwindcss'],
-//   tailwindFunctions: ['clsx'],
-// }
