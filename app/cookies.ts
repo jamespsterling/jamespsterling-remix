18f8d5842ba6058259c001cc50a3b3bf0c14ae7e
James Sterling <jamespsterling@users.noreply.github.com>
2026-01-25 14:28:08 -0500

    chore: correct cookie maxAge comment

--- app/cookies.ts
+++ app/cookies.ts
@@ -1,7 +1,7 @@
 import { createCookie } from "react-router";
 
 export const userPrefs = createCookie("user-prefs", {
-	maxAge: 10 * 365 * 24 * 60 * 60, // one year
+	maxAge: 10 * 365 * 24 * 60 * 60, // ten years
 	httpOnly: true,
 	sameSite: "strict",
 	secure: process.env.NODE_ENV === "production",
